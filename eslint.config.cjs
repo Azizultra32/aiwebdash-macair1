d8d0a520178f1e203d0d0cf233f9790c36d9e20d
AliGGGG <123049964+Azizultra32@users.noreply.github.com>
2025-05-20 08:25:51 -0700

    chore: update eslint config for flat style

--- eslint.config.cjs
+++ eslint.config.cjs
@@ -1,46 +1,53 @@
-module.exports = {
-  env: { browser: true, es2020: true },
-  extends: [
-    'eslint:recommended',
-    'plugin:react/recommended',
-    'plugin:react-hooks/recommended',
-    'plugin:import/recommended',
-    'plugin:jsx-a11y/recommended',
-    'plugin:@typescript-eslint/recommended',
-    'eslint-config-prettier',
-  ],
-  settings: {
-    react: {
-      // Tells eslint-plugin-react to automatically detect the version of React to use.
-      version: 'detect',
-    },
-    // Tells eslint how to resolve imports
-    'import/resolver': {
-      node: {
-        paths: ['src'],
-        extensions: ['.js', '.jsx', '.ts', '.tsx'],
-      },
-      typescript: {},
-    },
+const { FlatCompat } = require('@eslint/eslintrc');
+
+const compat = new FlatCompat();
+
+module.exports = [
+  {
+    ignores: ['node_modules/**', 'dist/**', 'env.d.ts', 'eslint.config.cjs'],
   },
-  ignores: ['node_modules/', 'dist/', 'env.d.ts'],
-  ignorePatterns: ['dist', 'eslint.config.cjs'],
-  parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh', 'import'],
-  rules: {
-    'react-refresh/only-export-components': 'off',
-    'react/react-in-jsx-scope': 'off',
-    'react/jsx-uses-react': 'off',
-    'react/prop-types': 'off',
-    '@typescript-eslint/no-explicit-any': 'off',
-    'react-hooks/exhaustive-deps': 'off',
-    '@typescript-eslint/no-unused-vars': [
-      'error',
-      {
-        argsIgnorePattern: '^_',
-        varsIgnorePattern: '^_',
-      },
+  ...compat.config({
+    env: { browser: true, es2020: true },
+    extends: [
+      'eslint:recommended',
+      'plugin:react/recommended',
+      'plugin:react-hooks/recommended',
+      'plugin:import/recommended',
+      'plugin:jsx-a11y/recommended',
+      'plugin:@typescript-eslint/recommended',
+      'eslint-config-prettier',
     ],
-    'import/named': 'off',
-  },
-};
+    settings: {
+      react: {
+        // Tells eslint-plugin-react to automatically detect the version of React to use.
+        version: 'detect',
+      },
+      // Tells eslint how to resolve imports
+      'import/resolver': {
+        node: {
+          paths: ['src'],
+          extensions: ['.js', '.jsx', '.ts', '.tsx'],
+        },
+        typescript: {},
+      },
+    },
+    parser: '@typescript-eslint/parser',
+    plugins: ['react-refresh', 'import'],
+    rules: {
+      'react-refresh/only-export-components': 'off',
+      'react/react-in-jsx-scope': 'off',
+      'react/jsx-uses-react': 'off',
+      'react/prop-types': 'off',
+      '@typescript-eslint/no-explicit-any': 'off',
+      'react-hooks/exhaustive-deps': 'off',
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        {
+          argsIgnorePattern: '^_',
+          varsIgnorePattern: '^_',
+        },
+      ],
+      'import/named': 'off',
+    },
+  }),
+];
